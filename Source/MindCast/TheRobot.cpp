ded03c9c502b2da609b95f637941e6ff3dfa4577
Benedikt <bendikorn@gmail.com>
2019-12-20 17:30:47 +0000

    Initial commit

---
+++ Source/MindCast/TheRobot.cpp
@@ -0,0 +1,106 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TheRobot.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values
+ATheRobot::ATheRobot()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RobotCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	RobotCamera->SetupAttachment(RootComponent);
+
+	Arms = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Arms"));
+	Arms->SetupAttachment(RobotCamera);
+}
+
+// Called when the game starts or when spawned
+void ATheRobot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATheRobot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	V = GetCharacterMovement()->Velocity;
+	m_speed = UKismetMathLibrary::Sqrt(UKismetMathLibrary::MultiplyMultiply_FloatFloat(V.X, 2.f) +
+		UKismetMathLibrary::MultiplyMultiply_FloatFloat(V.Y, 2.f) +
+		UKismetMathLibrary::MultiplyMultiply_FloatFloat(V.Z, 2.f));
+
+
+	UE_LOG(LogTemp, Warning, TEXT("Camera pitch: %f"), RobotCamera->GetRelativeTransform().Rotator().Pitch)
+}
+
+// Called to bind functionality to input
+void ATheRobot::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Forward_Backward", this, &ATheRobot::MoveForwardAxis);
+	PlayerInputComponent->BindAxis("LeftRight", this, &ATheRobot::MoveSidewaysAxis);
+	PlayerInputComponent->BindAxis("RotatePlayer", this, &ATheRobot::RotatePlayer);
+	PlayerInputComponent->BindAxis("RotateCamera", this, &ATheRobot::RotateCamera);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ATheRobot::Jump);
+	PlayerInputComponent->BindAction("Run", IE_Pressed, this, &ATheRobot::StartRunning);
+	PlayerInputComponent->BindAction("Run", IE_Released, this, &ATheRobot::StopRunning);
+
+}
+
+void ATheRobot::MoveForwardAxis(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector(), AxisValue);
+}
+
+void ATheRobot::MoveSidewaysAxis(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector(), AxisValue);
+}
+
+void ATheRobot::RotatePlayer(float AxisValue)
+{
+	AddControllerYawInput(AxisValue);
+}
+
+void ATheRobot::RotateCamera(float AxisValue)
+{
+
+	CameraPitch = RobotCamera->GetRelativeTransform().Rotator().Pitch;
+
+	if( CameraPitch >= -73.5f && CameraPitch <= 73.f)
+		RobotCamera->SetRelativeRotation(RobotCamera->GetRelativeTransform().Rotator() + FRotator(AxisValue, 0.0, 0.0));
+
+	else if (CameraPitch < -73.5f) RobotCamera->SetRelativeRotation(
+		FRotator(-72.f, RobotCamera->GetRelativeTransform().Rotator().Yaw, RobotCamera->GetRelativeTransform().Rotator().Roll));
+
+	else if (CameraPitch > 73.f) RobotCamera->SetRelativeRotation(
+		FRotator(72.5f, RobotCamera->GetRelativeTransform().Rotator().Yaw, RobotCamera->GetRelativeTransform().Rotator().Roll));
+	
+
+	//GetMesh()->SetBone
+
+	//-83.5 - 76.0
+}
+
+void ATheRobot::Jump()
+{
+	ACharacter::Jump();
+}
+
+void ATheRobot::StartRunning()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 1500.f;
+}
+
+void ATheRobot::StopRunning()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 600.f;
+}
---
+++ Source/MindCast/TheRobot.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "TheRobot.generated.h"
+
+UCLASS()
+class MINDCAST_API ATheRobot : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATheRobot();
+
+
+
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* RobotCamera;
+
+	UPROPERTY(EditAnywhere)
+		USkeletalMeshComponent* Arms;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void MoveForwardAxis(float AxisValue);
+	void MoveSidewaysAxis(float AxisValue);
+	void RotatePlayer(float AxisValue);
+	void RotateCamera(float AxisValue);
+	void Jump();
+	void StartRunning();
+	void StopRunning();
+
+	UCharacterMovementComponent* OurMovementComponent;
+
+private:
+
+	FVector V{ 0.f, 0.f, 0.f };
+	float m_speed{ 0.f };
+	float CameraPitch{ 0.f };
+};
